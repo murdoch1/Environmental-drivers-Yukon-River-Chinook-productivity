562faf95fc2a264c9ebe8c5384bf141c45a47fc8
murdoch1 <alyssamurdoch@gmail.com>
2022-03-01 12:59:33 -0500

    Intermediate code changes to calculate migration timing

--- Calculating Migration Timing Stats.R
+++ Calculating Migration Timing Stats.R
@@ -29,5 +29,41 @@ summarizedDOY
 popn=rep(c("LowerMainstem","White-Donjek","Pelly","Stewart","Carmacks","Teslin","MiddleMainstem","UpperMainstem"),times=(35))
 summarizedDOY[,"population"] <- popn
 
-years=rep(1985:2019,times=8)
+years=rep(1985:2019,each=8)
 summarizedDOY[,"year"] <- years
+
+write.csv(summarizedDOY,"Data/MigTiming_stats.csv",row.names=FALSE)
+
+#calculate interval needed by year
+
+Timing_interval_25_75 <- summarizedDOY %>%
+  group_by(year) %>%
+  summarize(min= min(q_25),
+            max=max(q_75)) %>%
+  data.frame()
+Timing_interval_25_75
+
+#list of day of year that fish are passing
+
+summarizedDOY_long <- arrayAsDF %>%
+  group_by(year, population) %>%
+  summarize(yday = rep(doy, Percent))
+
+#add stats
+
+summarizedDOY_long_int1 <- left_join(summarizedDOY_long,summarizedDOY)
+
+MigTiming_intervals_int1 <- summarizedDOY_long_int1 %>% 
+  filter(yday>=q_25&yday<=q_75) %>% 
+  mutate(yday_adj=yday-30) %>% 
+  select(1:2,9)
+
+#sub in year and population names
+levels(MigTiming_intervals_int1$year) <- 1985:2019
+levels(MigTiming_intervals_int1$population) <- (c("LowerMainstem","White-Donjek","Pelly","Stewart","Carmacks","Teslin","MiddleMainstem","UpperMainstem"))
+
+MigTiming_intervals <- distinct(MigTiming_intervals_int1)
+
+
+
+write.csv(summarizedDOY,"Data/MigTiming_stats.csv",row.names=FALSE)
